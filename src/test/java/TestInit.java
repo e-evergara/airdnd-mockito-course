c9a6543cfa6e305767386a39d79fcecd15e18b16
evergara <emersonjb@gmail.com>
2024-11-15 21:56:57 -0500

    Añadiendo librería para trabajar con Mockito y crear un test de prueba

---
+++ src/test/java/TestInit.java
@@ -0,0 +1,13 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestInit {
+    @Test
+    void testBooking() {
+        System.out.println("Booking test");
+        assertTrue(true);
+    }
+}
